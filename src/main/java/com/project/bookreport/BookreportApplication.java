b36165a15027d62b57fca1e6811477dd8ce69756
geunyoung490 <geunyoung490@naver.com>
2023-03-28 17:30:05 +0900

    project settings

---
+++ src/main/java/com/project/bookreport/BookreportApplication.java
@@ -0,0 +1,13 @@
+package com.project.bookreport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookreportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookreportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/bookreport/BookreportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.bookreport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookreportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
