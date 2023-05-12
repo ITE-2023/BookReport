27e5e870e7f1db592d5b2c9474397a098f739c31
jyajoo <jyajoo1020@gmail.com>
2023-05-12 17:20:00 +0900

    feat: JWTAuthorizationFilter 추가

--- src/main/java/com/project/bookreport/security/SecurityConfig.java
+++ src/main/java/com/project/bookreport/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.project.bookreport.security;
 
+import com.project.bookreport.security.jwt.JWTAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity // 스프링 시큐리티 사용을 위한 어노테이션
 @RequiredArgsConstructor
@@ -26,7 +28,12 @@ public class SecurityConfig {
         .httpBasic().disable()   // Http basic Auth 기반 로그인 기능 사용 X
         .formLogin().disable()   // security form 로그인 사용 X
         .sessionManagement()
-        .sessionCreationPolicy(SessionCreationPolicy.STATELESS); // 스프링 시큐리티가 생성하지도 않고 존재해도 사용하지 않음
+        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 스프링 시큐리티가 생성하지도 않고 존재해도 사용하지 않음
+        .and()
+        .addFilterBefore(
+            new JWTAuthorizationFilter(authenticationConfiguration.getAuthenticationManager()),
+            UsernamePasswordAuthenticationFilter.class);
+
     return http.build();
   }
 }
