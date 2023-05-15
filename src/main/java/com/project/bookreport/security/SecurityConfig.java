683fcf6fc312009772e0086dd5e8423bf303f9ab
jyajoo <jyajoo1020@gmail.com>
2023-05-15 16:01:58 +0900

    feat: h2-console 추가

--- src/main/java/com/project/bookreport/security/SecurityConfig.java
+++ src/main/java/com/project/bookreport/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @EnableWebSecurity // 스프링 시큐리티 사용을 위한 어노테이션
 @RequiredArgsConstructor
@@ -26,11 +27,14 @@ public class SecurityConfig {
       AuthenticationConfiguration authenticationConfiguration) throws Exception {
     http
         .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+            .requestMatchers(new AntPathRequestMatcher("/h2-console/**")).permitAll()
             .requestMatchers("/member/join", "/member/login", "/member/reissue").permitAll()
             .anyRequest().authenticated())
         .cors().disable()       // 타 도메인에서 API 접근 허용
         .csrf().disable()       // CSRF 토큰 기능 사용 X
         .httpBasic().disable()   // Http basic Auth 기반 로그인 기능 사용 X
+        .headers().frameOptions().sameOrigin()
+        .and()
         .formLogin().disable()   // security form 로그인 사용 X
         .sessionManagement()
         .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 스프링 시큐리티가 생성하지도 않고 존재해도 사용하지 않음
